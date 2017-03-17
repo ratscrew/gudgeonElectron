19aa930b68e80fb87a54d645eff84acff0446ab4
ratscrew <ratscrew2002@yahoo.com>
2017-03-17 13:27:45 +0000

    update

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
 const electron = require('electron')
+const config = require('./config.json')
 // Module to control application life.
 const app = electron.app
+const Menu = electron.Menu;
+const dialog = electron.dialog;
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
-
+const fs = require('fs');
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 var mainWindow
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600,autoHideMenuBar:true, webPreferences :{nodeIntegration:false}})
+  mainWindow = new BrowserWindow({ width: 1000, height: 700, title: config.title, autoHideMenuBar: config.autoHideMenuBar, webPreferences: { nodeIntegration: false } }) //, webPreferences :{nodeIntegration:false}
+  mainWindow.maximize()
 
   // and load the index.html of the app.
-  mainWindow.loadURL('http://localhost:3000/#/group/571e513b0c726a3268db766e/sheet/572a11f30c726a17a0cd5ca5') //'http://mlc.crowell.com/Console/#/inbox/'
-
+  mainWindow.loadURL(config.url) //'http://mlc.crowell.com/Console/#/inbox/' //http://localhost:3000/#/group/571e513b0c726a3268db766e/sheet/572a11f30c726a17a0cd5ca5
+  mainWindow.show()
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  //mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
@@ -51,3 +55,123 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+const template = [
+  {
+    label: 'Edit',
+    submenu: [
+      {
+        label: 'Undo',
+        accelerator: 'CmdOrCtrl+Z',
+        role: 'undo'
+      },
+      {
+        label: 'Redo',
+        accelerator: 'Shift+CmdOrCtrl+Z',
+        role: 'redo'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Cut',
+        accelerator: 'CmdOrCtrl+X',
+        role: 'cut'
+      },
+      {
+        label: 'Copy',
+        accelerator: 'CmdOrCtrl+C',
+        role: 'copy'
+      },
+      {
+        label: 'Paste',
+        accelerator: 'CmdOrCtrl+V',
+        role: 'paste'
+      },
+      {
+        label: 'Select All',
+        accelerator: 'CmdOrCtrl+A',
+        role: 'selectall'
+      },
+      {
+        label: 'Save to PDF',
+        accelerator: 'CmdOrCtrl+P',
+        role: 'saveToPDF',
+        click(item, focusedWindow) {
+          //focusedWindow.webContents.print({printBackground:true})
+          dialog.showSaveDialog(focusedWindow, {
+            filters: [
+              { name: 'PDF', extensions: ['pdf'] }
+            ]
+          }, function (filePath) {
+            focusedWindow.webContents.printToPDF({ printBackground: true }, (error, data) => {
+              if (error) throw error;
+              fs.writeFile(filePath, data, (error) => {
+                if (error)
+                  throw error;
+              });
+            });
+          })
+
+        }
+      }
+    ]
+  },
+  {
+    label: 'View',
+    submenu: [
+      {
+        label: 'Reload',
+        accelerator: 'CmdOrCtrl+R',
+        click(item, focusedWindow) {
+          if (focusedWindow) focusedWindow.reload();
+        }
+      },
+      {
+        label: 'Toggle Full Screen',
+        accelerator: process.platform === 'darwin' ? 'Ctrl+Command+F' : 'F11',
+        click(item, focusedWindow) {
+          if (focusedWindow)
+            focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+        }
+      },
+      {
+        label: 'Toggle Developer Tools',
+        accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+        click(item, focusedWindow) {
+          if (focusedWindow)
+            focusedWindow.webContents.toggleDevTools();
+        }
+      },
+    ]
+  },
+  {
+    label: 'Window',
+    role: 'window',
+    submenu: [
+      {
+        label: 'Minimize',
+        accelerator: 'CmdOrCtrl+M',
+        role: 'minimize'
+      },
+      {
+        label: 'Close',
+        accelerator: 'CmdOrCtrl+W',
+        role: 'close'
+      },
+    ]
+  },
+  {
+    label: 'Help',
+    role: 'help',
+    submenu: [
+      {
+        label: 'Learn More',
+        click() { require('electron').shell.openExternal('http://electron.atom.io'); }
+      },
+    ]
+  },
+];
+
+const menu = Menu.buildFromTemplate(template);
+Menu.setApplicationMenu(menu);
